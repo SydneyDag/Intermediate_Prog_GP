fcd7e241e6a94a62ec8173604ee659a1a67157ed
SydneyDag <85245088+SydneyDag@users.noreply.github.com>
2024-10-29 19:58:40 -0400

    Added to implementation file
    changed header.cpp to header.h

---
+++ implementation.cpp
@@ -0,0 +1,28 @@
+
+#include"header.h"
+#include <iostream>
+using namespace std;
+
+bool lineType::nonVertical(double a, double b, double c){
+    
+}
+
+double lineType::slope(double a, double b, double c){
+
+}
+
+bool lineType::lineEquality(lineType line1, lineType line2){
+
+}
+
+bool lineType::parallelLines(lineType line1, lineType line2){
+
+}
+
+bool lineType::perpendicularLines(lineType line1, lineType line2){
+
+}
+
+bool lineType::intersectionPoint(lineType line1, lineType line2){
+
+}
